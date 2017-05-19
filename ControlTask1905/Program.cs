2a41ae8036f295a7ad703ac666bbc40ab637f48e
Oleksandr <xander_uzzzer@mail.ru>
2017-05-19 20:13:40 +0300

    Add project files.

---
+++ ControlTask1905/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlTask1905
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
